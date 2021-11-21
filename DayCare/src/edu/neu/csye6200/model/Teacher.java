0ae6e806be8159413d5068c98d994af356064fda
Sutiksh Verma <verma.sutiksh27@gmail.com>
2021-11-21 16:09:30 -0500

    Update teacher age functionality

--- DayCare/src/edu/neu/csye6200/model/Teacher.java
+++ DayCare/src/edu/neu/csye6200/model/Teacher.java
@@ -35,7 +35,7 @@ public class Teacher extends Person {
 		Teacher t = new Teacher();
 		t.setFirstName(attributes[0]);
 		t.setLastName(attributes[1]);
-		t.setAge(Integer.parseInt(attributes[2]));
+		t.setAge((int)(Integer.parseInt(attributes[2])/ 12));
 		t.setAddress(attributes[3]);
 		t.setPhoneNum(Integer.parseInt(attributes[4]));
 		t.setReview(Double.parseDouble(attributes[5]));
