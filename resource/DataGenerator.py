4320ac70f1701f6a6aff2fc06449e35c006c2b9b
Niro Lin <425060468@qq.com>
2021-11-21 15:09:38 -0500

    The CSV data and the generator code

---
+++ resource/DataGenerator.py
@@ -0,0 +1,85 @@
+import csv
+import random
+
+StudentNum = 300
+TeacherNum = 50
+
+Title_P = ["FirstName", "LastName", "Age", "Id", "Address", "PhoneNum", "ParentName"]
+Title_S = ["immunizationDate", "registrationDate", "studentId"]
+Title_T = ["review", "employeeId"]
+
+IdBase = 1000000
+SidBase = 2000000
+TidBase = 3000000
+PhoneBase = 781000000
+
+
+def load_names(name_file):
+    names = []
+    f = open(name_file)
+    for line in f.readlines():
+        info = line.strip().split()
+        names.extend(info)
+
+    return names
+
+
+def generate_student_data(filename):
+    student_writer = csv.writer(open(filename, "w", newline=''), dialect="excel")
+    student_writer.writerow(Title_P + Title_S)
+    pid = IdBase
+    stu_id = SidBase
+    phone_num = PhoneBase
+
+    for i in range(StudentNum):
+        name_idx = random.randint(0, len(Names) - 1)
+        first_name = Names[name_idx]
+        last_name = Names[(name_idx + 1000) % len(Names)]
+        age = random.randint(18, 25)
+        pid += random.randint(1, 11)
+        address = Names[(name_idx + 1500) % len(Names)] + " Ave " + str(random.randint(1, 500))
+        phone_num += random.randint(1, 1000)
+        parent_name = Names[(name_idx + 2000) % len(Names)]
+
+        immunization_date = str(random.randint(1, 13)) + "-" + str(random.randint(1, 28)) + "-2020"
+        registration_date = str(random.randint(1, 13)) + "-" + str(random.randint(1, 28)) + "-" \
+                            + str(random.randint(2018, 2022))
+        # gpa = "%.1f" % (2 + random.random() * 2.0)
+        stu_id += random.randint(1, 11)
+
+        info = [first_name, last_name, age, pid, address, phone_num, parent_name,
+                immunization_date, registration_date, stu_id]
+
+        student_writer.writerow(info)
+
+
+def generate_teacher_data(filename):
+    teacher_writer = csv.writer(open(filename, "w", newline=''), dialect="excel")
+    teacher_writer.writerow(Title_P + Title_T)
+    pid = IdBase + 3000000
+    t_id = TidBase
+    phone_num = PhoneBase + 500000
+
+    for i in range(TeacherNum):
+        name_idx = random.randint(0, len(Names) - 1)
+        first_name = Names[name_idx]
+        last_name = Names[(name_idx + 1000) % len(Names)]
+        age = random.randint(30, 60)
+        pid += random.randint(1, 11)
+        address = Names[(name_idx + 1500) % len(Names)] + " Ave " + str(random.randint(1, 500))
+        phone_num += random.randint(1, 1000)
+        parent_name = Names[(name_idx + 2000) % len(Names)]
+
+        review = 0.0
+        t_id += random.randint(1, 11)
+
+        info = [first_name, last_name, age, pid, address, phone_num, parent_name,
+                review, t_id]
+
+        teacher_writer.writerow(info)
+
+
+if __name__ == '__main__':
+    Names = load_names("names.txt")
+    generate_student_data("student_data.csv")
+    generate_teacher_data("teachers_data.csv")
